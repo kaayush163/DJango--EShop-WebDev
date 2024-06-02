599fb9cebe7b2dd5c2af9f0b0594608be585cccc
kaayush163 <kaayush163>
2024-06-03 00:09:44 +0530

    html file render using templates folder

--- store/views.py
+++ store/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     # print("request recieved")
-    return HttpResponse('<h1>Index Page</h1>')
+    # return HttpResponse('<h1>Index Page</h1>')
+    # return render(request,'index.html')
+    return render(request,'orders/order.html') #if want to render through the the subfolders of templates folder
+
