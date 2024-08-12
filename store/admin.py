d990193918c4eb54be86263f6aa7d44228f047a8
kaayush163 <kaayush163>
2024-08-12 12:31:02 +0530

    Model form for check out created

--- store/admin.py
+++ store/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
 from .models.customer import Customer
+from .models.orders import Order
+
 #SHowing table view with chngaing category object 1 to something else in admin url
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name','price','category'] #whatever defiend in model should be named same here also
@@ -17,3 +19,4 @@ class AdminCategory(admin.ModelAdmin):
 admin.site.register(Product,AdminProduct)  # by adding one more parameter adminProduct we can able to see int able view at admin url
 admin.site.register(Category, AdminCategory)
 admin.site.register(Customer)
+admin.site.register(Order)
